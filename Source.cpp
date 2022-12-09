f6a20f51b2d9e488fea32c98ccd985d3793c8f95
Sogdiiana <119744104+Sogdiiana@users.noreply.github.com>
2022-12-09 21:58:05 +0500

    Add files via upload

---
+++ Source.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+	int a, b;
+	cout << "enter the number -- ";
+	cin >> a;
+	b = a % 2;
+	if (b == 0)
+	{
+		cout << "  even number!!";
+
+	}
+	else
+	{
+		cout << "  odd number!!";
+	}
+
+
+
+
+	return 0;
+}
